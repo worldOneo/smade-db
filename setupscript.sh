3d2a19c45507343f948f144d5097872a199eb4fc
worldOneo <62426504+worldOneo@users.noreply.github.com>
2024-05-16 11:57:01 +0200

    Safe builds take way to long

--- setupscript.sh
+++ setupscript.sh
@@ -8,7 +8,7 @@ wget https://ziglang.org/download/0.11.0/zig-linux-x86_64-0.11.0.tar.xz
 tar -xf zig-linux-x86_64-0.11.0.tar.xz
 git clone https://github.com/worldOneo/smade-db
 cd smade-db
-../zig-linux-x86_64-0.11.0/zig build-exe -O ReleaseSafe -mcpu native ./src/main.zig
+../zig-linux-x86_64-0.11.0/zig build-exe -O ReleaseFast -mcpu native ./src/main.zig
 ../zig-linux-x86_64-0.11.0/zig build-exe -O ReleaseFast -mcpu native ./src/loader.zig
 
 service dragonfly stop
